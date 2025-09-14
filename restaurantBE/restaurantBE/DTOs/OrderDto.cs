5a3ca8e79ae2a6d112d63a70a7e2f7870ae903c9
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:52:53 +0300

    Add OrderDto

---
+++ restaurantBE/restaurantBE/DTOs/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurantBE.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public decimal Total { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public List<DishDto> Dishes { get; set; } = new();
+    }
+}
