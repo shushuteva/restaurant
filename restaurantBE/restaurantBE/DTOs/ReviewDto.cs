b0570ac437baac74d06be319352bc888b22da2db
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:53:10 +0300

    Add ReviewDto

---
+++ restaurantBE/restaurantBE/DTOs/ReviewDto.cs
@@ -0,0 +1,12 @@
+﻿namespace restaurantBE.DTOs
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public int RestaurantId { get; set; }
+    }
+}
