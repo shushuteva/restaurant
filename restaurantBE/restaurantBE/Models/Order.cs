d1bc33f73a265fb20e1c729b518f814fce87fd61
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:53:53 +0300

    Add Order model

---
+++ restaurantBE/restaurantBE/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace restaurantBE.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public decimal Total { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public string Status { get; set; } = "pending";
+
+        public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
+    }
+}
