ab03fdea828fb370b3d9c1936d61e5012f7da29b
Victor Mitev <victormitev23@gmail.com>
2025-09-14 18:47:28 +0300

    update AppDbContext

--- restaurantBE/restaurantBE/Data/AppDbContext.cs
+++ restaurantBE/restaurantBE/Data/AppDbContext.cs
@@ -9,10 +9,16 @@ namespace restaurantBE.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Feedback>()
+                .Property(m => m.Rating)
+                .HasColumnType("decimal(18,2)");
 
             modelBuilder.Entity<Dish>()
                 .Property(m => m.Price)
