1bc3e02b5e3a8b7b3a7d2c79544935b9b49b6989
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:51:40 +0300

    Add OrderCreateDto

---
+++ restaurantBE/restaurantBE/DTOs/OrderCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace restaurantBE.DTOs
+{
+    public class OrderCreateDto
+    {
+        public int UserId { get; set; }
+        public List<int> DishIds { get; set; } = new();
+        public string PaymentMethod { get; set; } = "cash";
+    }
+}
