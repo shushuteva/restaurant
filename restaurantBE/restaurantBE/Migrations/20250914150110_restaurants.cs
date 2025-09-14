b686576bb346abd86a666931bfa854d519538a6e
Victor Mitev <victormitev23@gmail.com>
2025-09-14 18:52:46 +0300

    add Restaurants Table Migration

---
+++ restaurantBE/restaurantBE/Migrations/20250914150110_restaurants.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace restaurantBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class restaurants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cuisine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_RestaurantId",
+                table: "Dishes",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dishes_Restaurants_RestaurantId",
+                table: "Dishes",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dishes_Restaurants_RestaurantId",
+                table: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dishes_RestaurantId",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Dishes");
+        }
+    }
+}
