96f104878e9fb35da1d13f25db0afc448e6d98d9
Victor Mitev <victormitev23@gmail.com>
2025-09-14 17:35:24 +0300

    Initial commit

---
+++ restaurantBE/restaurantBE/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restaurantBE.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace restaurantBE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Dish>()
+                .Property(m => m.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
