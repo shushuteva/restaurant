79915cd734f335c4839cdd24ed10df18ac1ee6ed
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:47:52 +0300

    Add Users, Orders and Reviews, update OnModelCreating

--- restaurantBE/restaurantBE/Data/AppDbContext.cs
+++ restaurantBE/restaurantBE/Data/AppDbContext.cs
@@ -11,6 +11,9 @@ namespace restaurantBE.Data
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -20,9 +23,24 @@ namespace restaurantBE.Data
                 .Property(m => m.Rating)
                 .HasColumnType("decimal(18,2)");
 
+            modelBuilder.Entity<Order>()
+                .Property(m => m.Total)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             modelBuilder.Entity<Dish>()
                 .Property(m => m.Price)
                 .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne(d => d.Restaurant)
+            .HasForeignKey(d => d.RestaurantId);
         }
     }
 }
