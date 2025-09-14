05e6a2837797e593b414d7994d3240fde268f554
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:54:23 +0300

    Add User model

---
+++ restaurantBE/restaurantBE/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace restaurantBE.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string PasswordHash { get; set; }
+
+        [Required]
+        public required string PasswordSalt { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
