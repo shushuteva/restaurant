03ba009006879ad3b74128a159d5d62806b71ee4
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:57:50 +0300

    Add validation

--- restaurantBE/restaurantBE/DTOs/RegisterUserDto.cs
+++ restaurantBE/restaurantBE/DTOs/RegisterUserDto.cs
@@ -1,9 +1,15 @@
-﻿namespace restaurantBE.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace restaurantBE.DTOs
 {
     public class RegisterUserDto
     {
+        [Required]
         public required string Name { get; set; }
+
+        [EmailAddress]
         public required string Email { get; set; }
+        [Required]
         public required string Password { get; set; }   
     }
 }
