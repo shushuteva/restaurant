938c1d72d35ac5cfbf486879dc63392823d12b94
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:54:08 +0300

    Add Review model

---
+++ restaurantBE/restaurantBE/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace restaurantBE.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int RestaurantId { get; set; }
+
+        [JsonIgnore]
+        public Restaurant Restaurant { get; set; }
+    }
+}
