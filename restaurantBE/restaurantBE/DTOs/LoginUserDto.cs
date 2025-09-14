3f7396590ec515bd710fb773efe17dad137fc6ed
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-09-14 19:50:56 +0300

    Add LoginUserDto

---
+++ restaurantBE/restaurantBE/DTOs/LoginUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace restaurantBE.DTOs
+{
+    public class LoginUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
